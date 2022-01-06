ee4112af27d03dc4c693a8cef2d79b2459d9e428
najme <ndashti882@gmail.com>
2022-01-06 13:12:02 +0330

    change timeToEat

--- 1/timetoeat.js
+++ 1/timetoeat.js
@@ -1,50 +1,56 @@
 function setDateTime(time) {
-    let date = new Date();
-    const index = time.indexOf(":");
-    const index2 = time.indexOf(" ");
+  let date = new Date();
+  const index = time.indexOf(":");
+  const index2 = time.indexOf(" ");
 
-    let hours = +time.substring(0, index);
-    const minutes = +time.substring(index + 1, index2);
+  let hours = +time.substring(0, index);
+  const minutes = +time.substring(index + 1, index2);
 
-    const mer = time.substring(index2 + 1, time.length);
-    if (mer === "p.m." && hours !== 12){
-        hours = hours + 12;
-    }
+  const mer = time.substring(index2 + 1, time.length);
+  if (mer === "p.m." && hours !== 12) {
+    hours = hours + 12;
+  } else if (mer === "a.m." && hours === 12) {
+    hours = hours - 12;
+  }
 
+  date.setHours(hours);
+  date.setMinutes(minutes);
+  date.setSeconds("00");
 
-    date.setHours(hours);
-    date.setMinutes(minutes);
-    date.setSeconds("00");
-
-    return date;
+  return date;
 }
-
 
 const timeToEat = (currnetTime) => {
-    const mealsTime = ["7:00 a.m.", "12:00 p.m." ,"7:00 p.m."];
+  const mealsTime = ["7:00 a.m.", "12:00 p.m.", "7:00 p.m."];
 
-    const currentDate = setDateTime(currnetTime);
-    const selectedDate = {};
-    
-    mealsTime.forEach(time => {
-        if(setDateTime(time).getTime() >= currentDate.getTime()){
-            selectedDate[time] = Math.abs((setDateTime(time).getHours() * 60 + setDateTime(time).getMinutes()) - ( currentDate.getHours() * 60 + currentDate.getMinutes()));
-        }
-    });
+  const currentDate = setDateTime(currnetTime);
+  const selectedDate = {};
 
-    let key = Object.keys(selectedDate).reduce((key, v) => selectedDate[v] < selectedDate[key] ? v : key);
-    const hour = Math.floor(selectedDate[key] / 60);
-    const minute = selectedDate[key] - (hour * 60)
+  mealsTime.forEach((time) => {
+    if (setDateTime(time).getTime() >= currentDate.getTime()) {
+      selectedDate[time] = Math.abs(
+        setDateTime(time).getHours() * 60 +
+          setDateTime(time).getMinutes() -
+          (currentDate.getHours() * 60 + currentDate.getMinutes())
+      );
+    }
+  });
 
-    return [hour, minute];
+  let key = Object.keys(selectedDate).reduce((key, v) =>
+    selectedDate[v] < selectedDate[key] ? v : key
+  );
+  const hour = Math.floor(selectedDate[key] / 60);
+  const minute = selectedDate[key] - hour * 60;
 
-}
+  return [hour, minute];
+};
 
-console.log(timeToEat('2:00 p.m.'));
-console.log(timeToEat('5:50 a.m.'));
-console.log(timeToEat('7:50 a.m.'));
-console.log(timeToEat('3:10 p.m.'));
-
+console.log(timeToEat("2:00 p.m."));
+console.log(timeToEat("5:50 a.m."));
+console.log(timeToEat("7:50 a.m."));
+console.log(timeToEat("3:10 p.m."));
+console.log(timeToEat("00:50 a.m."));
+console.log(timeToEat("12:50 a.m."));
 
 // function timeToEat(myDate){
 //     const inputArr=myDate.split(" ")
@@ -68,5 +74,5 @@ console.log(timeToEat('3:10 p.m.'));
 //     const res=minResult.map((item,index)=>{
 //         return Math.floor(item/60)
 //     })
-//     return res  
+//     return res
 // }
